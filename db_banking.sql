9c155c78a9c69529460d4b70ad4107fb325cd1bf
Tanishka Gaikwad <tanishkagaikwad918@gmail.com>
2026-07-28 12:19:32 +0530

    created database

---
+++ db_banking.sql
@@ -0,0 +1,12 @@
+show databases;
+
+use banking_db;
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    AccountCreationDate DATE
+);
+select * from customers;
