480a571a40d50f781b0dabd4641bc0df30aeb33b
Tanishka Gaikwad <tanishkagaikwad918@gmail.com>
2026-08-08 15:16:22 +0530

    used update, insert and delete commands

--- db_banking.sql
+++ db_banking.sql
@@ -107,3 +107,39 @@ ALTER TABLE Loans
 ADD CONSTRAINT FK_Loans_Customers
 FOREIGN KEY (CustomerID)
 REFERENCES Customers(CustomerID);
+
+INSERT INTO Customers
+(CustomerID, FirstName, LastName, Email, Phone, DateOfBirth)
+VALUES
+(101,'Rahul','Sharma','rahul@gmail.com','9876543210','1998-04-15');
+
+INSERT INTO Accounts
+(AccountID, CustomerID, AccountType, Balance)
+VALUES
+(201,101,'Savings',25000);
+
+SET SQL_SAFE_UPDATES=0;
+
+UPDATE Customers
+SET Phone='9999999999'
+WHERE CustomerID=101;
+
+SELECT * FROM Customers
+WHERE CustomerID = 101;
+
+UPDATE Customers
+SET Email='rahul.sharma@gmail.com'
+WHERE CustomerID=101;
+
+SELECT * FROM Customers
+WHERE CustomerID = 101;
+
+DELETE FROM Transactions
+WHERE TransactionID = 302;
+
+SELECT * FROM Transactions;
+
+DELETE FROM Accounts
+WHERE AccountID = 202;
+
+SELECT * FROM Accounts;
