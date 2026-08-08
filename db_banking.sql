cfc8a7b4c6da6d842c7991ef191ab8acb55892b3
Tanishka Gaikwad <tanishkagaikwad918@gmail.com>
2026-08-08 16:25:39 +0530

    Lab 4 added by using DQL select commands

--- db_banking.sql
+++ db_banking.sql
@@ -143,3 +143,56 @@ DELETE FROM Accounts
 WHERE AccountID = 202;
 
 SELECT * FROM Accounts;
+
+SELECT * FROM Customers;
+
+SELECT FirstName, LastName, Email, Phone
+FROM Customers;
+
+SELECT *
+FROM Accounts
+WHERE AccountType = 'Savings';
+
+SELECT *
+FROM Accounts
+WHERE Balance > 25000;
+
+SELECT *
+FROM Transactions
+WHERE Amount BETWEEN 5000 AND 20000;
+
+SELECT *
+FROM Customers
+WHERE CustomerID IN (101,102,103);
+
+SELECT *
+FROM Customers
+WHERE FirstName LIKE 'R%';
+
+SELECT *
+FROM Customers
+ORDER BY FirstName ASC;
+
+SELECT *
+FROM Accounts
+ORDER BY Balance DESC;
+
+SELECT DISTINCT AccountType
+FROM Accounts;
+
+SELECT *
+FROM Accounts
+ORDER BY Balance DESC
+LIMIT 3;
+
+SELECT *
+FROM Transactions
+LIMIT 5 OFFSET 2;
+
+SELECT *
+FROM Customers
+WHERE Phone IS NULL;
+
+SELECT *
+FROM Customers
+WHERE Email IS NOT NULL;
