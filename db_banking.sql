ea61c0e3ddb537566cbc28ad817cc5e1c677922a
Tanishka Gaikwad <tanishkagaikwad918@gmail.com>
2026-08-17 12:29:21 +0530

    lab 5 added & completed

--- db_banking.sql
+++ db_banking.sql
@@ -220,3 +220,68 @@ SELECT TransactionID,
        Amount,
        AVG(Amount) OVER () AS AverageTransaction
 FROM Transactions;
+
+SELECT *
+FROM Customers
+WHERE FirstName LIKE 'A%';
+
+SELECT *
+FROM Customers
+WHERE Email LIKE '%gmail%';
+
+SELECT *
+FROM Customers
+WHERE LastName LIKE '%kar';
+
+SELECT *
+FROM Accounts
+WHERE AccountType IN ('Savings', 'Current');
+
+SELECT *
+FROM Transactions
+WHERE TransactionType IN ('Deposit', 'Withdrawal');
+
+SELECT *
+FROM Customers
+WHERE CustomerID IN (101,102,105);
+
+SELECT *
+FROM Customers
+ORDER BY LastName ASC;
+
+SELECT *
+FROM Accounts
+ORDER BY Balance DESC;
+
+SELECT *
+FROM Transactions
+ORDER BY TransactionDate DESC;
+
+SELECT *
+FROM Accounts
+ORDER BY Balance DESC
+LIMIT 5;
+
+SELECT *
+FROM Customers
+LIMIT 3;
+
+SELECT *
+FROM Transactions
+LIMIT 5 OFFSET 3;
+
+SELECT *
+FROM Accounts
+WHERE AccountType = 'Savings'
+ORDER BY Balance DESC;
+
+SELECT *
+FROM Customers
+WHERE FirstName LIKE 'S%'
+LIMIT 5;
+
+SELECT *
+FROM Transactions
+WHERE TransactionType IN ('Deposit','Withdrawal')
+ORDER BY TransactionDate DESC;
+
